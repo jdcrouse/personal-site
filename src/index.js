43013229757bc583f48bf9c21a9d48affaa8f0b1
Jason Crouse <jcrouse@drift.com>
2021-05-29 13:03:26 -0400

    change page title and fix font

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-<link rel="stylesheet" href="https://use.typekit.net/dnh0ust.css"></link>;
 
 ReactDOM.render(
   <React.StrictMode>
